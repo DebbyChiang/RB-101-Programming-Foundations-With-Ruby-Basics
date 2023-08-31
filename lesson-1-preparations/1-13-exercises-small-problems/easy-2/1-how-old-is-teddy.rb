90035b15ada39b637f84532f64e318b3c93cb8a7
Debby Chiang <debbychiang85226@gmail.com>
2023-08-31 11:12:22 -0700

    started easy-2 exercises

---
+++ lesson-1-preparations/1-13-exercises-small-problems/easy-2/1-how-old-is-teddy.rb
@@ -0,0 +1,15 @@
+#Build a program that randomly generates and 
+#prints Teddy's age. To get the age, you should 
+#generate a random number between 20 and 200.
+
+# Example Output
+
+# Teddy is 69 years old!
+
+#create age variable
+#create sentence with string interpolation
+
+age = rand(20..200)
+
+puts "Teddy is #{age} years old!"
+
