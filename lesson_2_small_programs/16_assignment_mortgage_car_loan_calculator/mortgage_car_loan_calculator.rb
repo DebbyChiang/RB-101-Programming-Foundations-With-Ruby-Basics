4370e51abcf4f10c3f22244ce8eb1a2a785c7260
Debby Chiang <debbychiang85226@gmail.com>
2023-09-04 16:17:46 -0700

    calculator updates

--- lesson_2_small_programs/16_assignment_mortgage_car_loan_calculator/mortgage_car_loan_calculator.rb
+++ lesson_2_small_programs/16_assignment_mortgage_car_loan_calculator/mortgage_car_loan_calculator.rb
@@ -1,21 +1,37 @@
-puts "Welcome to Mortgage / Car Loan Calculator!" 
+puts 'Welcome to Mortgage / Car Loan Calculator!'
 
 loan_amount = ''
 loop do
-  puts "Please enter your loan amount (i.e. 50000 for $50000) Disclude the $ sign or other non-numeric characters.:"
-  loan_amount = gets.chomp.delete("$").delete(",")
-  if loan_amount.empty?() || loan_amount.to_f() < 0
-    puts "Must enter positive number."
+  puts 'Please enter your loan amount (i.e. 50000 for $50000) Disclude the $ sign or other non-numeric characters.:'
+  loan_amount = gets.chomp.delete('$').delete(',')
+  if loan_amount.empty? || loan_amount.to_f.negative?
+    puts 'Must enter positive number.'
   else
     break
   end
 end
 
-puts "Please enter your Annual Percentage Rate (APR) (i.e. 5.0 for 5%):"
-apr = gets.chomp.to_f
+apr = ''
+loop do
+  puts 'Please enter your Annual Percentage Rate (APR) (i.e. 5.0 for 5%):'
+  apr = gets.chomp.to_f
+  if loan_amount.empty? || loan_amount.to_f.negative?
+    puts 'Must enter positive number.'
+  else
+    break
+  end
+end
 
-puts "Please enter your loan duration in years (i.e. 10):"
-loan_duration = gets.chomp.to_i
+loan_duration = ''
+loop do
+  puts 'Please enter your loan duration in years (i.e. 10):'
+  loan_duration = gets.chomp.to_i
+  if loan_amount.empty? || loan_amount.to_f.negative?
+    puts 'Must enter positive number.'
+  else
+    break
+  end
+end
 
 monthly_interest_rate_visual = apr / 12
 monthly_interest_rate_decimal = monthly_interest_rate_visual / 100
@@ -24,5 +40,7 @@ puts "Your monthly interest rate is #{monthly_interest_rate_visual.round(2)}%."
 loan_duration_months = loan_duration * 12
 puts "Your loan duration is: #{loan_duration_months} months."
 
-monthly_payment = loan_amount.to_f * (monthly_interest_rate_decimal / (1-(1 + monthly_interest_rate_decimal)** (-loan_duration_months)))
+monthly_payment = loan_amount.to_f *
+                  (monthly_interest_rate_decimal /
+                  (1 - (1 + monthly_interest_rate_decimal)**-loan_duration_months))
 puts "Your monthly payment is: $#{monthly_payment.round(2)}"
