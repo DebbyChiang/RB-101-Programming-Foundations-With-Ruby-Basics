92ebfa0bb8459745a017e1207192a7ee24c8ad54
Debby Chiang <debbychiang85226@gmail.com>
2023-09-11 19:37:43 -0700

    updates

--- lesson_3_practice_problems/4_practice_problems_medium_1/question_5.rb
+++ lesson_3_practice_problems/4_practice_problems_medium_1/question_5.rb
@@ -6,11 +6,13 @@
 # Ben coded up this implementation but complained that as soon as 
 # he ran it, he got an error. Something about the limit variable. 
 # What's wrong with the code?
+#The limit variable is outside the scope of the method.
+#Place the limit as an argument
 
 # Copy Code
-limit = 15
 
-def fib(first_num, second_num)
+
+def fib(first_num, second_num, limit = 15)
   while first_num + second_num < limit
     sum = first_num + second_num
     first_num = second_num
@@ -22,3 +24,4 @@ end
 result = fib(0, 1)
 puts "result is #{result}"
 # How would you fix this so that it works?
+#place the limit as an argument within the function
